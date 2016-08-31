382f3f7b3776527769cf4733dd2c1b826ec483a3
Joe Kim <jjinking@gmail.com>
2016-08-31 23:23:22 +0000

    feat: Set up the directory structure to enable workflow of developing ng2 app on flask server
    - created script copy2static.sh to copy built angular app to flask static directory
    - modified flask template index.html to load the copied js, css, and img files
    - modified yeoman generated angular app to be able to deal with the static file structure by putting images in a directory named ‘static’ within src/public

---
+++ angular2/copy2static.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+STATIC_DIR='../server/static'
+
+# Copy images
+cp dist/static/img/* $STATIC_DIR/img/
+
+# Copy css
+cp dist/css/app.*.css $STATIC_DIR/css/app.css
+
+# Copy js files
+cp dist/js/app.*.js    $STATIC_DIR/js/app.js
+cp dist/js/common.*.js $STATIC_DIR/js/common.js
+cp dist/js/vendor.*.js $STATIC_DIR/js/vendor.js
+
