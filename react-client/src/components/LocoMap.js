d96b8515d8bfb804c5b2efe0013a559ec237bf2f
Joseph <jjinking@gmail.com>
2016-09-21 19:48:29 -0400

    feat: Added google-maps-react npm module
    App shows map on the page now

--- react-client/src/components/LocoMap.js
+++ react-client/src/components/LocoMap.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {connect} from 'react-redux';
+import {Map, Marker, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
 
 import * as actions from '../actions';
 
@@ -10,17 +11,37 @@ export class LocoMap extends React.Component {
   constructor(props) {
     super(props);
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    this.state = {showingInfoWindow: false};
   }
 
   getLocos() {
     return this.props.locos || [];
   }
+
+  // A point on the map is clicked
+  onMapClicked(mapProps, map, clickEvent) {
+    console.log(clickEvent.latLng.lat(), clickEvent.latLng.lng());
+    this.setState({showingInfoWindow: true});
+  }
+
+  // Marker is clicked
+  onMarkerClick(props, marker, e) {
+  }
   
   render() {
     return (
       <div className="map">
-	This is where the map goes
-
+	
+	<Map google={window.google} onClick={this.onMapClicked.bind(this)}>
+	  <Marker onClick={this.onMarkerClick} name={'Current location'} />
+	  <InfoWindow visible={this.state.showingInfoWindow} onClose={this.onInfoWindowClose}>
+	    <div>
+	      <h1>{/*this.state.selectedPlace.name*/}</h1>
+	      <h1>Hello</h1>
+	    </div>
+	  </InfoWindow>
+	</Map>
+	
 	<ul>
 	  {this.getLocos().map(loco => <li key={loco.name}>{loco.name}, {loco.coord}</li>)}
 	</ul>
@@ -30,6 +51,11 @@ export class LocoMap extends React.Component {
   
 }
 
+// Set Google Map api
+const LocoMapKey = GoogleApiWrapper({
+  apiKey: 'AIzaSyApYKQNcnzFoSa4TP-l7PB4IoDm-pyT66w'
+})(LocoMap)
+
 
 // Set up redux-connected component
 const mapStateToProps = (state) => {
@@ -37,4 +63,4 @@ const mapStateToProps = (state) => {
     locos: state.get('locos')
   };
 }
-export const LocoMapContainer = connect(mapStateToProps, actions)(LocoMap);
+export const LocoMapContainer = connect(mapStateToProps, actions)(LocoMapKey);
